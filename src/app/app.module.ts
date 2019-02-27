1fb0a850212df91768f2a41d153aa73ec437a311
LucaSchere <luca@scherer.ml>
2019-02-27 22:02:13 +0100

    intro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IntroComponent } from './components/intro/intro.component';
+import { IntroScreenComponent } from './components/intro-screen/intro-screen.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroComponent,
+    IntroScreenComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
